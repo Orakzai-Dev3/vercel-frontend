c071d92e5f7838dd55f5ac23e3f97c5faf5f4361
Orakzai-Dev3 <arehman.dev3@gmail.com>
2025-05-02 03:49:23 +0500

    something has been edited

--- src/components/Card_slide_women.jsx
+++ src/components/Card_slide_women.jsx
@@ -10,7 +10,7 @@ function Card_slide_women({img,category}) {
         <div className='relative w-[100%] h-full'>
           <img className='w-full' src={img} alt=",," />
           {/* Absolute positioned button at bottom-right of image */}
-          <div className='absolute bottom-4 right-2 bg-[#CA9154] rounded-md py-3 px-2 duration-300 opacity-0  group-hover:opacity-100 transition-opacity'>
+          <div className='absolute bottom-4 right-2 bg-[#CA9154] rounded-md py-1 sm:py-2 md:py-3 px-2 duration-300 opacity-0  group-hover:opacity-100 transition-opacity'>
             <h1 className='inline-block'>See products</h1>
             <img className='inline-block pl-3' src="https://cdn.leatherscin.com/img/cart-icon.webp" alt="" />
           </div>
